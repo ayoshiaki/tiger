64f4ee95f6acec2b31e3361695fb76e8ba012cf6
WDelfino <Wallace Delfino>
2014-09-04 03:44:07 -0300

    Wallace Delfino - Minor bugs
    -Correção do flag que faz o jump para false (L1-correto), antes o mesmo
    estava apontando para o caso verdadeiro (L0-errado);
    -Remoção dos casos inúteis das classes CJUMP, Print e Codegen, podendo
    evitar possíveis bugs;
    -Supressão dos códigos de Dump que estavam sendo exibidos, facilitando
    a visualização do console. Caso alguém queria ativa-los para debug
    basta descomentar a linha 113 da classe RegAlloc :)

--- src/tiger/mips/Codegen.java
+++ src/tiger/mips/Codegen.java
@@ -138,23 +138,11 @@ public class Codegen {
             case CJUMP.GE:
                 emit(new OPER("bgez $t3,L0", null, null, new LabelList(label_aux, new LabelList(s.iftrue, new LabelList(s.iffalse, null)))));
                 break;
-            /*case CJUMP.ULT:
-                emit(new OPER("JB `j0 ; munchCJump", null, null, new LabelList(label_aux, new LabelList(s.iftrue, new LabelList(s.iffalse, null)))));
-                break;
-            case CJUMP.ULE:
-                emit(new OPER("JBE `j0; munchCJump", null, null, new LabelList(label_aux, new LabelList(s.iftrue, new LabelList(s.iffalse, null)))));
-                break;
-            case CJUMP.UGT:
-                emit(new OPER("JA `j0 ; munchCJump", null, null, new LabelList(label_aux, new LabelList(s.iftrue, new LabelList(s.iffalse, null)))));
-                break;
-            case CJUMP.UGE:
-                emit(new OPER("JAE `j0; munchCJump", null, null, new LabelList(label_aux, new LabelList(s.iftrue, new LabelList(s.iffalse, null)))));
-                break;*/
         }
 
         /* faz o jump para false */
         emit(new tiger.assem.LABEL(label_aux.toString() + ":", label_aux));
-        emit(new OPER("j L0", null, null, new LabelList(s.iftrue, null)));
+        emit(new OPER("j L1", null, null, new LabelList(s.iftrue, null)));
 
     }
 
