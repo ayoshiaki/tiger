99b9744ebd211d851ad3230237127ac717e41da6
matheusmollon <matheusmollon@hotmail.com>
2014-10-08 21:30:39 -0300

    Implementação dos métodos abstrados de Exp e Stm e duas classes implementadas

--- src/tiger/tree/Exp.java
+++ src/tiger/tree/Exp.java
@@ -2,4 +2,5 @@ package tiger.tree;
 abstract public class Exp {
 	abstract public ExpList kids();
 	abstract public Exp build(ExpList kids);
+        abstract public void accept(Visitor v, int d);
 }
