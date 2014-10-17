99b28bbb63946ceb654731db1ab04dd4dd711882
Thales Martins <linkthales@hotmail.com>
2014-10-17 17:38:37 -0300

    Accpet do UEXP implementado

--- src/tiger/tree/Print.java
+++ src/tiger/tree/Print.java
@@ -86,7 +86,7 @@ public class Print implements Visitor {
 
     @Override
     public void visit(UEXP var) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        var.accept(this);
     }
 
     public Print(java.io.PrintWriter o, TempMap t) {
