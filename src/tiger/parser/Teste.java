ae20f6c59eee66d4127947d4853499f9011d45fc
btguilherme <btguilherme@msn.com>
2014-12-08 19:16:59 -0200

    desenvolvendo interface de teste

---
+++ src/tiger/parser/Teste.java
@@ -0,0 +1,28 @@
+package tiger.parser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Teste extends javax.swing.JInternalFrame {
+
+    /**
+     * Creates new form Teste
+     */
+    public Teste() {
+        initComponents();
+        this.setClosable(true);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+    
