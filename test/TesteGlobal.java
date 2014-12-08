e94f0e385d74c1c8acd7c542d55aa275161ecd04
LookPlace <gabriel-yuri10@hotmail.com>
2014-12-08 16:04:18 -0200

    inicio de teste semantico

---
+++ test/TesteGlobal.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.IOException;
+import java.util.logging.Level;
+import static java.util.logging.Level.SEVERE;
+import java.util.logging.Logger;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import tiger.parser.TigerLexer;
+import tiger.parser.TigerParser;
+import tiger.semant.SemantVisitor;
+import tiger.translate.Frag;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class TesteGlobal {
+    private String path;
+    
+    public TesteGlobal() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    //Testes semanticos 
+    @Test
+    public void test20(){
+        TigerLexer lex;
+      
+        try {
+
+        } catch (IOException ex) {
+            Logger.getLogger(TesteGlobal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
